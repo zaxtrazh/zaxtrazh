a623c09b6ba71a1ea444cc3d1d903a13c1b5de37
zaxtrazh <45304150+zaxtrazh@users.noreply.github.com>
2023-06-22 14:04:33 +0700

    Add files via upload

---
+++ dasar_if.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 5;
+	
+	if(x <= 5)
+ 	{
+      cout<<"nilai x kurang dari = 5"<<endl;
+	}
+	else if(x <= 10)
+	{
+      cout<<"nilai x kurang dari 10"<<endl;
+	}
+	else
+	{
+ 	   cout<<"nilai x lebih dari 10"<<endl;
+   }
+	
+	system("pause");
+	return 0;
+}
